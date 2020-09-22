c013737d72326d621f0b8294bcc662aced54837f
vlakreeh <zeb@zebulon.dev>
2020-09-22 14:13:24 -0400

    Add sidebar tooltips

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import SidebarIcon from "./SidebarIcon";
+import ReactTooltip from "react-tooltip";
 import "./Sidebar.css"
 
-
 export default function Sidebar() {
-    return (<div id="sidebar"></div>);
+    return (<div id="sidebar">
+        <SidebarIcon src="code-download-outline" dataFor="remap" />
+        <ReactTooltip id="remap" effect="solid" place="right" backgroundColor="#313131" delayShow={400}>
+            <span> Remap the input binary and download the result </span>
+        </ReactTooltip>
+        <SidebarIcon src="settings-2-outline" dataFor="settings" />
+        <ReactTooltip id="settings" effect="solid" place="right" backgroundColor="#313131" delayShow={400}>
+            <span> Configure the remapper </span>
+        </ReactTooltip>
+        <SidebarIcon src="save-outline" dataFor="save" />
+        <ReactTooltip id="save" effect="solid" place="right" backgroundColor="#313131" delayShow={400}>
+            <span> Save the current input and reference binaries to local storage </span>
+        </ReactTooltip>
+    </div>);
 }
--- src/component/sidebar/SidebarIcon.tsx
+++ src/component/sidebar/SidebarIcon.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SidebarIcon.css";
+
+const eva = require("eva-icons");
 
 export type SidebarIconProps = {
     src: string,
     alt?: string,
-    onClick: () => void,
+    dataFor?: string,
+    onClick?: () => void,
 };
 
-export default function SidebarIcon({ src, alt, onClick }: SidebarIconProps) {
-    return (<div onClick={onClick} className="sidebarIcon">
-        <img src={src} alt={alt}/>
+export default function SidebarIcon({ src, alt, dataFor, onClick }: SidebarIconProps) {
+    useEffect(() => eva.replace(), []);
+    return (<div onClick={onClick} data-tip="" data-for={dataFor} className="sidebarIcon">
+        <img data-eva={src} alt={alt} />
     </div>);
 }
